77e4ea0de10de28824f13264dfcc36f3d3ac180e
Daria Kukuruza <dashakukuruza86@gmail.com>
2024-02-23 20:41:05 +0200

    Add project files.

---
+++ Filtration/Program.cs
@@ -0,0 +1,218 @@
+﻿
+using System.Collections.Generic;
+
+class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public int DepId { get; set; }
+}
+class Department
+{
+    public int Id { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+}
+class Program
+{
+    static void Main()
+    {
+        List<Department> departments = new List<Department>()
+ {
+new Department(){ Id = 1, Country = "Ukraine", City = "Lviv" },
+new Department(){ Id = 2, Country = "Ukraine", City = "Kyiv" },
+new Department(){ Id = 3, Country = "France", City = "Paris" },
+new Department(){ Id = 4, Country = "Ukraine", City = "Odesa" }
+ };
+        List<Employee> employees = new List<Employee>()
+{
+new Employee()
+{ Id = 1, FirstName = "Tamara", LastName = "Ivanova", Age = 22, DepId = 2 },
+new Employee()
+{ Id = 2, FirstName = "Nikita", LastName = "Larin", Age = 33, DepId = 1 },
+new Employee()
+{ Id = 3, FirstName = "Alica", LastName = "Ivanova", Age = 43, DepId = 3 },
+new Employee()
+{ Id = 4, FirstName = "Lida", LastName = "Marusyk", Age = 22, DepId = 2 },
+new Employee()
+{ Id = 5, FirstName = "Lida", LastName = "Voron", Age = 36, DepId = 4 },
+new Employee()
+{ Id = 6, FirstName = "Ivan", LastName = "Kalyta", Age = 22, DepId = 2 },
+new Employee()
+{ Id = 7, FirstName = "Nikita", LastName = " Krotov ", Age = 27, DepId = 4 },
+new Employee()
+{ Id = 7, FirstName = "Dima", LastName = " Lipov ", Age = 27, DepId = 2 }
+};
+
+
+        while (true)
+        {
+            Console.WriteLine("\nВыберите номер задания для проверки:");
+            Console.WriteLine("1) Выбрать имена и фамилии сотрудников, работающих в Украине, но не в Одессе.");
+            Console.WriteLine("2) Вывести список стран без повторений.");
+            Console.WriteLine("3) Выбрать 3-x первых сотрудников, возраст которых превышает 25 лет.");
+            Console.WriteLine("4) Выбрать имена, фамилии и возраст студентов из Киева, возраст которых превышает 23 года.");
+            Console.WriteLine("5) Выйти из программы.");
+
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 5)
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите число от 1 до 5.");
+            }
+
+            if (choice == 5)
+            {
+                Console.WriteLine("Выход из программы...");
+                break;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    SelectEmployeesNotInOdesa(employees, departments);
+                    break;
+                case 2:
+                    PrintUniqueCountries(employees, departments);
+                    break;
+                case 3:
+                    SelectFirstThreeOver25(employees, departments);
+                    break;
+                case 4:
+                    SelectKyivResidentsOver23(employees, departments);
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор. Пожалуйста, выберите номер задания от 1 до 4.");
+                    break;
+            }
+
+        }
+    }
+
+    private static void SelectKyivResidentsOver23(List<Employee> employees, List<Department> departments)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Список жителей Киева старше 23 лет через синтаксис запросов LINQ:");
+        var user = from e in employees
+                    join d in departments on e.DepId equals d.Id
+                    where d.City == "Kyiv" && e.Age > 23
+                    select new { e.FirstName, e.LastName, e.Age };
+
+        foreach (var person in user)
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName}, возраст: {person.Age}");
+        }
+        Console.ResetColor();
+
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Список жителей Киева старше 23 лет через методы расширения LINQ:");
+        var u = employees.Join(departments, e => e.DepId, d => d.Id, (e, d) => new { e.FirstName, e.LastName, e.Age, d.City })
+                                     .Where(x => x.City == "Kyiv" && x.Age > 23)
+                                     .Select(x => new { x.FirstName, x.LastName, x.Age });
+
+        foreach (var person in u)
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName}, возраст: {person.Age}");
+        }
+        Console.ResetColor();
+    }
+
+
+    private static void SelectFirstThreeOver25(List<Employee> people, List<Department> departments)
+    {
+        var querySyntax = (from e in people
+                           where e.Age > 25
+                           orderby e.Age
+                           select e).Take(3);
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Первые три сотрудника старше 25 лет через синтаксис запросов LINQ:");
+        foreach (var employee in querySyntax)
+        {
+            Console.WriteLine($"{employee.FirstName} {employee.LastName}, Возраст: {employee.Age}");
+        }
+        Console.ResetColor();
+
+        Console.WriteLine();
+
+        var methodSyntax = people.Where(e => e.Age > 25)
+                                    .OrderBy(e => e.Age)
+                                    .Take(3);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Первые три сотрудника старше 25 лет через методы расширения LINQ:");
+        foreach (var employee in methodSyntax)
+        {
+            Console.WriteLine($"{employee.FirstName} {employee.LastName}, Возраст: {employee.Age}");
+        }
+        Console.ResetColor();
+
+    }
+
+    private static void PrintUniqueCountries(List<Employee> people, List<Department> departments)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Список стран без повторений через синтаксис запросов LINQ:");
+
+        var query = (from c in departments
+                     select c.Country).Distinct();
+        foreach (var country in query)
+        {
+            Console.WriteLine(country);
+        }
+
+        Console.ResetColor();
+
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Список стран без повторений через методы расширения LINQ:");
+
+        var uniqueCountries = departments.Select(c => c.Country).Distinct();
+        foreach (var country in uniqueCountries)
+        {
+            Console.WriteLine(country);
+        }
+        Console.ResetColor();
+    }
+
+    private static void SelectEmployeesNotInOdesa(List<Employee> employees, List<Department> departments)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Имена и фамилии сотрудников, работающих в Украине, но не в Одессе через синтаксис запросов LINQ:");
+        var query = from employee in employees
+                    join department in departments on employee.DepId equals department.Id
+                    where department.Country == "Ukraine" && department.City != "Odesa"
+                    group employee by new { employee.FirstName, employee.LastName, department.Country, department.City } into grouped
+                    select grouped;
+
+        foreach (var group in query)
+        {
+            Console.WriteLine($"{group.Key.LastName} {group.Key.FirstName} {group.Key.Country} {group.Key.City}");
+        }
+        Console.ResetColor();
+
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Имена и фамилии сотрудников, работающих в Украине, но не в Одессе через методы расширения LINQ:");
+
+        var result = employees
+       .Join(departments,
+             employee => employee.DepId,
+             department => department.Id,
+             (employee, department) => new { Employee = employee, Department = department })
+       .Where(x => x.Department.Country == "Ukraine" && x.Department.City != "Odesa")
+       .GroupBy(x => new { x.Employee.FirstName, x.Employee.LastName, x.Department.Country, x.Department.City })
+       .Select(group => group.Key);
+
+        foreach (var item in result)
+        {
+            Console.WriteLine($"{item.LastName} {item.FirstName} {item.Country} {item.City}");
+        }
+        Console.ResetColor();
+    }
+
+}
---
+++ LINQ_HW/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQ_HW
+{
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+   public  class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> person = new List<Person>()
+{
+new Person(){ Name = "Andrey", Age = 24, City = "Kyiv"},
+new Person(){ Name = "Liza", Age = 18, City = "Odesa" },
+new Person(){ Name = "Oleg", Age = 15, City = "London" },
+new Person(){ Name = "Sergey", Age = 55, City = "Kyiv" },
+new Person(){ Name = "Sergey", Age = 32, City = "Lviv" }
+};
+
+            while (true)
+            {
+            Console.WriteLine("Выберите задание для проверки:");
+            Console.WriteLine("1) Выбрать людей, старших 25 лет.");
+            Console.WriteLine("2) Выбрать людей, проживающих не в Лондоне.");
+            Console.WriteLine("3) Выбрать имена людей, проживающих в Киеве.");
+            Console.WriteLine("4) Выбрать людей, старших 35 лет, с именем Sergey.");
+            Console.WriteLine("5) Выбрать людей, проживающих в Одессе.");
+            Console.WriteLine("10) Выход");
+
+                int choice;
+                while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 5)
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите число от 1 до 5.");
+                }
+
+                Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        FilterPersonsByAge25AndOlder(person);
+                        break;
+                    case 2:
+                        FilterPersonsNotInLondon(person);
+                        break;
+                    case 3:
+                        FilterNamesInKyiv(person);
+                        break;
+                    case 4:
+                        FilterPersonsOver35NamedSergey(person);
+                        break;
+                    case 5:
+                        FilterPersonsInOdessa(person);
+                        break;
+                    default:
+                        Console.WriteLine("Неверный выбор. Пожалуйста, выберите номер задания от 1 до 5.");
+                        break;
+                }
+            }
+        }
+
+        static void FilterPersonsByAge25AndOlder(List<Person> people)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Фильтрация людей старше 25 лет через методы расширения LINQ:");
+
+            var Older = people.Where(person => person.Age > 25);
+
+            foreach (var i in Older)
+            {
+                Console.WriteLine(i.Name + " :" + i.Age);
+            }
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Фильтрация людей старше 25 лет через синтаксис запросов LINQ:");
+
+            var selectedUsers = from user in people
+                                where user.Age > 25
+                                select user;
+            foreach (var user in selectedUsers)
+            {
+                Console.WriteLine(user.Name + " :" + user.Age);
+            }
+            Console.ResetColor();
+        }
+
+        static void FilterPersonsNotInLondon(List<Person> people)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Фильтрация людей, не проживающих в Лондоне через методы расширения LINQ:");
+            var user = people.Where(person => person.City != "London");
+            foreach (var person in user)
+            {
+                Console.WriteLine(person.Name + " :" + person.City);
+            }
+
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Фильтрация людей, не проживающих в Лондоне через синтаксис запросов LINQ:");
+            var selectedUsers = from user2 in people
+                                where user2.City != "London"
+                                select user2;
+            foreach (var user2 in selectedUsers)
+            {
+                Console.WriteLine(user2.Name + " :" + user2.City);
+            }
+
+            Console.ResetColor();
+        }
+
+        static void FilterNamesInKyiv(List<Person> people)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Фильтрация имен людей, проживающих в Киеве через методы расширения LINQ:");
+
+            var usrName = people.Where(person => person.City == "Kyiv").Select(people => people.Name);
+            foreach (var user2 in usrName)
+            {
+                Console.WriteLine(user2);
+            }
+
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Фильтрация имен людей, проживающих в Киеве через синтаксис запросов LINQ:");
+            var Users = from user2 in people
+                    where user2.City == "Kyiv"
+                        select user2.Name;
+            foreach (var user2 in Users)
+            {
+                Console.WriteLine(user2);
+            }
+            Console.ResetColor();
+
+        }
+
+        static void FilterPersonsOver35NamedSergey(List<Person> people)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Фильтрация людей старше 35 лет с именем Sergey через методы расширения LINQ:");
+            var user = people.Where(person => person.Age > 35).Where(user => user.Name == "Sergey");
+            foreach (var person in user)
+            {
+                Console.WriteLine(person.Name + " :" + person.Age);
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Фильтрация людей старше 35 лет с именем Sergey через синтаксис запросов LINQ:");
+            var Users = from user2 in people
+                        where user2.Age > 35 && user2.Name == "Sergey"
+                        select user2;
+
+            foreach (var user2 in Users)
+            {
+                Console.WriteLine(user2.Name + " :" + user2.Age);
+            }
+            Console.ResetColor();
+        }
+
+        static void FilterPersonsInOdessa(List<Person> people)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Фильтрация людей, проживающих в Одессе через методы расширения LINQ:");
+            var user = people.Where(person => person.City == "Odesa");
+            foreach (var person in user)
+            {
+                Console.WriteLine(person.Name + " :" + person.Age + " - " + person.City);
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Фильтрация людей, проживающих в Одессе через синтаксис запросов LINQ:");
+            var Users = from user2 in people
+                        where user2.City == "Odesa"
+                        select user2;
+
+            foreach (var user2 in Users)
+            {
+                Console.WriteLine(user2.Name + " :" + user2.Age + " - " + user2.City);
+            }
+            Console.ResetColor();
+        }
+        
+
+            /*
+             Список используемых методов расширения LINQ
+
+                Select: определяет проекцию выбранных значений
+                Where: определяет фильтр выборки
+                OrderBy: упорядочивает элементы по возрастанию
+                OrderByDescending: упорядочивает элементы по убыванию
+                ThenBy: задает дополнительные критерии для упорядочивания элементов возрастанию
+                ThenByDescending: задает дополнительные критерии для упорядочивания элементов по убыванию
+                Join: соединяет две коллекции по определенному признаку
+                GroupBy: группирует элементы по ключу
+                ToLookup: группирует элементы по ключу, при этом все элементы добавляются в словарь
+                GroupJoin: выполняет одновременно соединение коллекций и группировку элементов по ключу
+                Reverse: располагает элементы в обратном порядке
+                All: определяет, все ли элементы коллекции удовлятворяют определенному условию
+                Any: определяет, удовлетворяет хотя бы один элемент коллекции определенному условию
+                Contains: определяет, содержит ли коллекция определенный элемент
+                Distinct: удаляет дублирующиеся элементы из коллекции
+                Except: возвращает разность двух коллекцию, то есть те элементы, которые содератся только в одной коллекции
+                Union: объединяет две однородные коллекции
+                Intersect: возвращает пересечение двух коллекций, то есть те элементы, которые встречаются в обоих коллекциях
+                Count: подсчитывает количество элементов коллекции, которые удовлетворяют определенному условию
+                Sum: подсчитывает сумму числовых значений в коллекции
+                Average: подсчитывает cреднее значение числовых значений в коллекции
+                Min: находит минимальное значение
+                Max: находит максимальное значение
+                Take: выбирает определенное количество элементов
+                Skip: пропускает определенное количество элементов
+                TakeWhile: возвращает цепочку элементов последовательности, до тех пор, пока условие истинно
+                SkipWhile: пропускает элементы в последовательности, пока они удовлетворяют заданному условию, и затем возвращает оставшиеся элементы
+                Concat: объединяет две коллекции
+                Zip: объединяет две коллекции в соответствии с определенным условием
+                First: выбирает первый элемент коллекции
+                FirstOrDefault: выбирает первый элемент коллекции или возвращает значение по умолчанию
+                Single: выбирает единственный элемент коллекции, если коллекция содердит больше или меньше одного элемента, то генерируется исключение
+                SingleOrDefault: выбирает первый элемент коллекции или возвращает значение по умолчанию
+                ElementAt: выбирает элемент последовательности по определенному индексу
+                ElementAtOrDefault: выбирает элемент коллекции по определенному индексу или возвращает значение по умолчанию, если индекс вне допустимого диапазона
+                Last: выбирает последний элемент коллекции
+                LastOrDefault: выбирает последний элемент коллекции или возвращает значение по умолчанию
+            */
+        }
+    }
+
